d5e294c9cb5d9a6678f941231be4c184dbdcfcd8
deepdhar <dhar2017.slg@gmail.com>
2021-07-13 13:00:57 +0530

    updated app

---
+++ app/src/main/java/com/example/mycontacts/activities/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.mycontacts.activities;
+
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.mycontacts.adapter.ContactsAdapter;
+import com.example.mycontacts.contentprovider.Contract;
+import com.example.mycontacts.R;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    public ContactsAdapter mContactsAdapter;
+    public static final  int CONTACTLOADER = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddContactActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ListView listView = findViewById(R.id.list);
+        mContactsAdapter = new ContactsAdapter(this, null);
+        listView.setAdapter(mContactsAdapter);
+
+        // whenever we press a listview for updating
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, AddContactActivity.class);
+                Uri newUri = ContentUris.withAppendedId(Contract.ContactEntry.CONTENT_URI, id);
+                intent.setData(newUri);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.fade_out);
+            }
+        });
+
+        // get the loader running
+        getLoaderManager().initLoader(CONTACTLOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        String[] projection = {Contract.ContactEntry._ID,
+                Contract.ContactEntry.COLUMN_NAME,
+                Contract.ContactEntry.COLUMN_EMAIL,
+                Contract.ContactEntry.COLUMN_PICTURE,
+                Contract.ContactEntry.COLUMN_PHONENUMBER,
+        };
+
+        return new CursorLoader(this, Contract.ContactEntry.CONTENT_URI,
+                projection, null,
+                null,
+                null);
+    }
+
+
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mContactsAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mContactsAdapter.swapCursor(null);
+    }
+}
